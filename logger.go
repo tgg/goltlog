303c938c8e54ad7888d6f8dde14f5b09bde327b7
Thomas Girard <thomas.g.girard@free.fr>
2014-10-21 01:47:49 +0200

    implement (or stub) remaining parts of Log

--- logger.go
+++ logger.go
@@ -5,12 +5,27 @@ import (
 	"unsafe"
 )
 
+var (
+	empty = make([]LogRecord, 0, 0)
+)
+
+func min(a, b int) int {
+   if a < b {
+      return a
+   }
+   return b
+}
+
 func currentTime() LogTime {
 	now := time.Now().UTC()
 	return LogTime{
-		Seconds: now.Unix(),
+		Seconds:     now.Unix(),
 		NanoSeconds: int64(now.Nanosecond()),
 	}
+}
+
+func (t LogTime) geq(o LogTime) bool {
+	return t.Seconds > o.Seconds || (t.Seconds == o.Seconds && t.NanoSeconds >= o.NanoSeconds)
 }
 
 type logger struct {
@@ -76,11 +91,71 @@ func (l *logger) GetAvailabilityStatus() (AvailabilityStatus, error) {
 	return st, nil
 }
 
+func (l *logger) GetRecordIdFromTime(t LogTime) (RecordId, error) {
+	if l.err != nil {
+		return RecordId(0), l.err
+	}
+	id := RecordId(len(l.records) + 1)
+	for _, r := range l.records {
+		if r.Time.geq(t) {
+			id = r.Id
+			break
+		}
+	}
+	return id, nil
+}
+
+func (l *logger) RetrieveRecords(currentId *RecordId, howMany *uint32) ([]LogRecord, error) {
+	if l.err != nil {
+		return nil, l.err
+	}
+	if uint64(*currentId) >= uint64(len(l.records)) {
+		return empty, nil
+	}
+	u := min(int(*currentId) + int(*howMany), len(l.records) - int(*currentId))
+	// Security risk: we return underlying structure.
+	r := l.records[int(*currentId):u]
+	*howMany = uint32(u - int(*currentId))
+	*currentId = RecordId(u + 1)
+	return r, nil
+}
+
+func (l *logger) RetrieveRecordsByLevel(currentId *RecordId, howMany *uint32, levels []LogLevel) ([]LogRecord, error) {
+	// TODO real implementation
+	return l.RetrieveRecords(currentId, howMany)
+}
+
+func (l *logger) RetrieveRecordsByProducerName(currentId *RecordId, howMany *uint32, names []string) ([]LogRecord, error) {
+	// TODO real implementation
+	return l.RetrieveRecords(currentId, howMany)
+}
+
+func (l *logger) RetrieveRecordsByProducerId(currentId *RecordId, howMany *uint32, ids []string) ([]LogRecord, error) {
+	// TODO real implementation
+	return l.RetrieveRecords(currentId, howMany)
+}
+
 func (l *logger) SetMaxSize(s uint64) error {
 	if l.err != nil {
 		return l.err
 	}
 	l.maxSize = s
+	return nil
+}
+
+func (l *logger) SetLogFullAction(a LogFullAction) error {
+	if l.err != nil {
+		return l.err
+	}
+	l.lfa = a
+	return nil
+}
+
+func (l *logger) SetAdministrativeState(s AdministrativeState) error {
+	if l.err != nil {
+		return l.err
+	}
+	l.as = s
 	return nil
 }
 
